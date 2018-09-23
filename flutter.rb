8b54de22f64131acae56b366724a2c0f368f81d7
eclewlow <32854625+eclewlow@users.noreply.github.com>
2018-09-23 01:56:02 -0700

    Update flutter.rb

--- flutter.rb
+++ flutter.rb
@@ -1,14 +1,16 @@
 class Flutter < Formula
   desc "Build beautiful native apps in record time"
   homepage "https://flutter.io/"
-  url "https://storage.googleapis.com/flutter_infra/releases/beta/macos/flutter_macos_v0.7.3-beta.zip"
-  sha256 "72ee026522be9cfc552b02886c9697f8bee0626635bc425b41b64c01e5253565"
+  version "1.0.0"
+  url "https://github.com/flutter/flutter.git"
+  sha256 "6e3ba2da94fb150d2cb8e0596eeb92c86e5e437e91a5f39f211bd4b21d0b24f8"
 
   bottle :unneeded
 
   def install
     allfiles = File.join(buildpath, "**", "{*,.*}")
     mv Dir.glob(allfiles), Dir.glob(prefix), :force => true
+    bin.install File.join(prefix, "bin/cache/dart-sdk/bin/dart")
   end
 
   def post_install
